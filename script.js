f470b04ecc29b8d52fbf54d7da5d74840434c609
kieran-amin <112574793+kieran-amin@users.noreply.github.com>
2024-09-24 12:00:25 -0400

    FirstUploadfromMac

---
+++ script.js
@@ -0,0 +1,6 @@
+// API Key = 932600eaf9aebc1894731da1aa9d116d
+
+// URL = https://api.openweathermap.org/data/2.5/weather?q=raleigh&appid=932600eaf9aebc1894731da1aa9d116d
+
+//API Call Format = https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
